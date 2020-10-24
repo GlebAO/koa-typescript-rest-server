5ac8befb9265b3b6923c88322f16803969e83a3c
GlebAO <shake@spb.rsnet.ru>
2020-10-24 21:03:35 +0300

    prod fixes

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 // Certificate
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/ecotermplus.ru-0003/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/ecotermplus.ru-0003/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/ecotermplus.ru-0003/chain.pem;', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/ecotermplus.ru-0003/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
